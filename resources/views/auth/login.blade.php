65952dc383b58824180a7fb6980a9cb24d88b9b2
Andre Soares <andreg.soares.dev@gmail.com>
2020-10-14 19:40:12 -0300

    ajustes tela de login, dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,23 +6,23 @@
         <form class="card auth_form" method="POST" action="{{ route('login') }}">
         @csrf
             <div class="header">
-                <img class="logo" src="{{asset('assets/images/logo.svg')}}" alt="">
-                <h5>Log in</h5>
+                <img class="logo" src="{{asset('images/logo.png')}}" alt="">
+                <h5>@lang('index.app_name')</h5>
             </div>
             <div class="body">
                 <div class="input-group mb-3">
-                    <input type="email" name="email" class="form-control" placeholder="Email">
+                    <input type="email" name="email" class="form-control" placeholder="@lang('login.email')">
                     <div class="input-group-append">
                         <span class="input-group-text"><i class="zmdi zmdi-account-circle"></i></span>
                     </div>
                 </div>
                 <div class="input-group mb-3">
-                    <input type="password" name="password" class="form-control" placeholder="Password">
+                    <input type="password" name="password" class="form-control" placeholder="@lang('login.password')">
                     <div class="input-group-append">
                         <span class="input-group-text"><i class="zmdi zmdi-lock"></i></span>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary btn-block waves-effect waves-light">Entrar</button>
+                <button type="submit" class="btn btn-primary btn-block waves-effect waves-light">@lang('login.login')</button>
             </div>
         </form>
         <div class="copyright text-center">
